4fcb7e66eee604cf3f0d2744dcb2091491a83b06
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2023-01-15 20:00:16 +0100

    comint

--- db/migrate/20230115103403_create_articles.rb
+++ db/migrate/20230115103403_create_articles.rb
@@ -8,3 +8,21 @@ class CreateArticles < ActiveRecord::Migration[7.0]
     end
   end
 end
+
+
+  #       Chec this if you delited migration
+
+      #remove    db/migrate/QQQQQ_create_entries.rb
+      #remove    app/models/entry.rb
+      #invoke    test_unit
+      #remove      test/models/entry_test.rb
+      #remove      test/fixtures/entries.yml
+#///////////////////////////////////////////////////////////////////////////////
+
+#У Ruby on Rails есть консоль. Запустим её:
+
+#rails console и введём в неё:
+
+#"Contact.all" Чтобы узнать какие свойства (поля) у сущности (модели) введём:
+
+#Contact.attribute_names
