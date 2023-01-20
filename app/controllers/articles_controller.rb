52089c4da3014d5df0b1b00cb1fd7778efa6516d
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2023-01-20 16:38:07 +0100

    working version

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,26 +1,47 @@
 class ArticlesController < ApplicationController
- 
-   def index   # Wywod wsech zapisej!
+    
+   def index   # 4: Wywod wsech zapisej!
       @articles = Article.all
    end
-
-   def show  # Wywodim bazu po :ID
+    
+   def show  # 3: Wywodim bazu po :ID
      @article = Article.find(params[:id])
    end
 
-   def new  #создать - new (отобразить форму. GET)
+   def new  # 1: создать - new (отобразить форму. GET)
    end
- 
-   def create #create (отправить форму. POST)   
+    
+   def create # 2: create (отправить форму. POST)   
        @article = Article.new(article_params)
     if @article.valid?
        @article.save 
        redirect_to @article #У нас происходит редирект на "show" поэтому представление --
                             #--"create" нам теперь не нужно, его можно удалить.
     else
-       render action: 'new'
+       render action: 'new'  #"perenaprowlenie"
      end
    end
+    
+   def edit   # 5: Wozwrat i Redactirowanie
+    @article = Article.find(params[:id])
+   end
+
+   def update #6  Wnosim izmenrnie w redaktirowanie
+    @article = Article.find(params[:id])
+  
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render action: 'redact'    #"perenaprowlenie"
+    end
+  end
+
+  def destroy # Delite publikacij
+    @article = Article.find(params[:id])
+    @article.destroy
+  
+    redirect_to action: "index" #"perenaprowlenie"
+  end
  #////////////////////////////////////////////////////////////////////////////////////
    private
  
