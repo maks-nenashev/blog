d765b38913ffa9d8df9ece1528e8f72861c01edf
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2023-01-28 18:45:59 +0100

    work version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+    
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+  
+    def configure_permitted_parameters
+    
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    end
+
+
 end
+
+
+
+#Все контроллеры наследуются от базового контроллера ApplicationController и чтобы задать 
+#для всех контроллеров один параметр, надо прописать это в ApplicationController.
+
+#before_action (в ранних версиях Rails это было before_filter), 
+#фильтрует методы в контроллерах до того как их обработать.
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+         # Podkluchenie Awtorizacii
+  devise_for :users
+
+  devise_scope :user do  
+      get '/users/sign_out' => 'devise/sessions#destroy'     
+   end
   
+   root to: "home#index"
+  #//////////////////////////////////////////////////////////////////////
   get '/' =>'home#index'    # Eto nuzno wsegda!
   #get 'home/index'
   
